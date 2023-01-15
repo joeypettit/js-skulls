17edc789e509b83896ad0a8a6643008b7d2b74ef
Joey Pettit <pettitbass@gmail.com>
2023-01-15 01:01:03 -0600

    added event listener for socket in gamestate provider

--- src/Contexts/GameStateProvider.jsx
+++ src/Contexts/GameStateProvider.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
+import { useSocket } from "./SocketProvider";
 
 const GameStateContext = React.createContext();
 
@@ -7,6 +8,26 @@ export function useGameState() {
 }
 
 export function GameStateProvider({ id, children }) {
+  const socket = useSocket();
+
+  function updateGameState() {
+    // update game state
+  }
+
+  // set up listeners for gamestate updates
+  useEffect(() => {
+    // if we do not have a socket, do nothing
+    if (socket == null) return;
+    // create recieve message socket event listener
+    // when message recieve pass arguments to addMessageToConversation
+    socket.on("update-gamestate", updateGameState);
+
+    // clean up: remove event listener when
+    return () => {
+      socket.off("update-gamestate");
+    };
+  }, [socket, addMessageToConversation]);
+
   const gameState = {};
   return (
     <GameStateContext.Provider value={gameState}>
