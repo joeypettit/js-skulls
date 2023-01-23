ac024672b741c5e0547f6b85cd0f87cf8d6c6507
Joey Pettit <pettitbass@gmail.com>
2023-01-22 23:07:12 -0600

    put different gamePhase buttons in their own nested components

---
+++ src/GameComponents/PlayOrBetButtons.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+function PlayOrBetButtons({ gameState, setShowBetting, setShowHand }) {
+  return (
+    <>
+      <Button
+        className="end-50 w-50"
+        variant="info"
+        onClick={() => setShowBetting(true)}
+        disabled={gameState.gamePhase === "Play or Bet" ? false : true}
+      >
+        Make a Bet
+      </Button>
+      <Button
+        variant="warning"
+        className="start-50 w-50"
+        onClick={() => setShowHand(true)}
+      >
+        Show Hand
+      </Button>
+    </>
+  );
+}
+
+export default PlayOrBetButtons;
